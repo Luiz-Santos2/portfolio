d39016a5d11947b4deb93fce176a10320c251851
Luiz Clovis Dos Santos Matias <luizclovis2014@gmail.com>
2024-06-08 19:48:55 -0300

    Ajuste da tag

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -16,7 +16,7 @@ const Projects: Array<Project> = [
         Description: "Informative application about pregnancy, childbirth and postpartum.",
         RepositoryURL: "https://github.com/Luiz-Santos2/meu-parto/tree/firebase",
         ProjectURL: "https://expo.dev/preview/update?message=Realizado%20ajuste%20em%20todo%20o%20app%20e%20retirado%20o%20build%20do%20github%20actions&updateRuntimeVersion=1.0.0&createdAt=2024-06-01T01%3A04%3A24.744Z&slug=exp&projectId=a8cd90a2-b424-415a-bab0-e60a456b8363&group=5f531c2a-bc70-4225-9d1a-a66fa729a513",
-        Tags: ["#React Native ", "#Typescript ", "#Expo ", "#GitHubActions ", "#Vercel "],
+        Tags: ["#React Native ", "#Typescript ", "#Expo ", "#GitHubActions "],
         SkillsIcons: "react,ts,githubactions"
     },
     {
@@ -34,7 +34,7 @@ const Projects: Array<Project> = [
         Description: "Professional presentation portfolio",
         RepositoryURL: "https://github.com/Luiz-Santos2/portfolio",
         ProjectURL: "https://portfolio-luiz-clovis.vercel.app/",
-        Tags: ["#Nextjs ", "#Typescript ", "#GitHubActions "],
+        Tags: ["#Nextjs ", "#Typescript ", "#GitHubActions ", "#Vercel "],
         SkillsIcons: "nextjs,ts,githubactions,vercel"
     },
  
